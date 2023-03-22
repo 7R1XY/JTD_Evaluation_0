5ae836c3e619d0245cbf879da67c476d3498d1df
Mohammed Sufiyan <116337828+7R1XY@users.noreply.github.com>
2023-03-22 13:42:51 +0530

    Add files via upload
    JTBB_BOOTCAMP
    EVALUATION_0

---
+++ Evaluation0.java
@@ -0,0 +1,174 @@
+/*Write a java program to replace all lowercase letter to uppercase  and vice versa
+             Input : str = “Great PoWer”
+            Output : str = “gREAT pOwER”
+                PROBLEM NUMBER-4 --------------------------------------------------------------------------------------------
+
+
+
+
+*/
+class toLowerCase{
+public static void main(String[] args){
+    int txt = ("Great PoWer");
+    System.out.println(txt.toLowerCase());
+    System.out.println(txt.toUpperCase());
+}
+}
+
+
+
+/*
+Find a string whether it contains alphanumeric characters or not?
+-----------------------------------------------------------------------
+           PROBLEM NUMBER - 2
+
+
+class main{
+public static void main(String[] args){
+    int str = ();
+    if ()
+    
+}
+}
+*/
+
+
+
+
+/* Write a java program to generate prime numbers from a range of  0 to n, Check sum of two prime numbers  is equal target value. Print two values
+Input : n = 20;
+Output: 3, 17
+           PROBLEM NUMBER-10
+---------------------------------------------------------------------------------------------------
+*/
+
+/*
+Write an algorithm to reverse a string
+              PROBLEM NUMBER - 01
+--------------------------------------------------
+           
+class main{
+public static void main(String[] args){
+    String a="SUFIYAN";
+    String bag="";
+   for(int i=0;i<a.length();i++){
+   bag=a.charAt(i)+bag;
+}
+System.out.println(bag);	
+
+}
+}
+*/ 
+
+
+/*
+Write a java program to print Below pattern
+Input : [[*_*_*],[_*_*].[*_*_*]]
+               PROBLEM NUMBER - 08
+---------------------------------------------------
+class Main {
+public static void main(String[] args) {
+   int  n=2;
+   for(int i=0;i<=n;i++){
+   String bag="";
+   for(int j=0;j<=n;j++){
+   if(i==0||i==n){
+   bag=bag+"*"+" ";
+    }else if(j==0||j==n){
+    bag=bag+"*"+" ";
+    }else {
+    bag=bag+"  ";
+    }
+    }
+    System.out.println(bag);
+        
+}
+}
+}
+
+*/
+
+
+/*
+Java program to convert integer into binary number system
+Input : 42
+Output : 101010
+                    PROBLEM NUMBER - 06
+-------------------------------------------------------------
+
+
+public class Demo {
+   public static void main( String args[] ) {
+      int num = 42;
+      String bag = Integer.toBinaryString(num);
+      System.out.println(bag);
+   }
+}
+*/
+
+
+/*
+Given two strings find if string1 is reverse of string2
+          PROBLEM NUMBER - 13
+-------------------------------------------------------------
+
+class main{
+public static void main(String[] args){
+    String n="SUFIYAN";
+    String b="SUFIYAN";
+    String bag="";
+   for(int i=0;i<n.length();i++){
+   bag=a.charAt(i)+bag;
+}
+System.out.println(bag);	
+
+}
+}
+*/
+/*
+Given  3 sides of a triangle, find out if the triangle is a right angled triangle.
+Right angled triangle = (side1)^2 + (side2)^2 = (side3)^2
+           PROBLEM NUMBER - 12
+---------------------------------------------------------------------
+
+class main{
+  public static void main(String[] args){
+  int x = 8;
+  int y = 6;
+  int z = 10; 
+  if ((x*x)+(y*y)==(z*z)){
+      System.out.println("Right angled triangle");
+   }
+    else{
+       System.out.println("Not a right angled traingle");
+     }
+  }
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
